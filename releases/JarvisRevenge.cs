996ce2b76c744e257db9aafc6f6b2ed7604b1f68
Vinicios Lugli <vinicioslugli@gmail.com>
2021-07-20 14:46:04 -0300

    Update robot info

--- releases/JarvisRevenge.cs
+++ releases/JarvisRevenge.cs
@@ -60,9 +60,9 @@ public static class Log{
 }
 public static class Robot{
 	//Private robot info. current robo-3:
-	public const byte kLights = 3;//number of sensors
-	public const byte kUltrassonics = 2;//number of sensors
-	public const byte kRefreshRate = 31;//ms of refresh rate in color/light sensor
+	public const byte kLights = 5;//number of sensors
+	public const byte kUltrassonics = 3;//number of sensors
+	public const byte kRefreshRate = 63;//ms of refresh rate in color/light sensor
 	//
 
 	public static void throwError(object message) => bc.RobotError(message.ToString());
