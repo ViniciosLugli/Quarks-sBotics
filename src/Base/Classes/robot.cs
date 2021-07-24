f5297edfadfba3122f01d47d6ef2702ab62d3d03
Vinicios Lugli <vinicioslugli@gmail.com>
2021-07-24 16:16:00 -0300

    Update robot class

--- src/Base/Classes/robot.cs
+++ src/Base/Classes/robot.cs
@@ -1,9 +1,9 @@
 public static class Robot{
 	//Private robot info. current robo-3:
-	public const byte kLights = 5;//number of sensors
+	public const byte kLights = 6;//number of sensors
 	public const byte kUltrassonics = 3;//number of sensors
-	public const byte kRefreshRate = 63;//ms of refresh rate in color/light sensor
-	public const byte ksize = 56;
+	public const byte kRefreshRate = 75;//ms of refresh rate in color/light sensor
+	public const byte ksize = 55;
 	//
 
 	public static void throwError(object message) => bc.RobotError(message.ToString());
